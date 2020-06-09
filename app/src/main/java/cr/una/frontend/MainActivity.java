0ee05a6e77b0916262c90f2be4385f1c1e939937
Miguel <m.sanchez397s@gmail.com>
2020-06-09 17:21:06 -0600

    loginview done

--- app/src/main/java/cr/una/frontend/MainActivity.java
+++ app/src/main/java/cr/una/frontend/MainActivity.java
@@ -1,13 +1,27 @@
 package cr.una.frontend;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText idTxt;
+    private EditText passTxt;
+    private Button logInBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init() {
+        idTxt = (EditText) findViewById(R.id.idTxt);
+        passTxt = (EditText) findViewById(R.id.passwordTxt);
+        logInBtn = (Button) findViewById(R.id.logInBtn);
     }
 }
---
+++ app/src/main/java/cr/una/frontend/controller/SignIn.java
@@ -0,0 +1,7 @@
+package cr.una.frontend.controller;
+
+import android.app.Activity;
+
+public class SignIn extends Activity {
+
+}
